f97edaf209759d8749b557257fab8fb51efd4408
Shakib Bin Kabir <shakibbinkabir@gmail.com>
2025-10-02 22:25:07 +0600

    Add initial .gitignore and index.php file with basic HTML structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SharkScope - Team Bionauts</title>
+</head>
+<body>
+    <h1>Welcome to SharkScope!</h1>
+    <p>Team Bionauts reporting for duty. The server is working!</p>
+    <p>Current PHP version: <?php echo phpversion(); ?></p>
+</body>
+</html>
